9ef30a14eaf9b95fe40c32ba7ef26312255888de
Admin <valeyk21@gmail.com>
2021-09-27 08:39:04 +0400

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Aleksey Nevskikh");
+        System.out.println("13.04.1987");
+    }
+
+}
