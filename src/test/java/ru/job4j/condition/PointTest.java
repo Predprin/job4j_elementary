91c4742e751c6d2cd508cebf7f88634fb23efa4b
Admin <valeyk21@gmail.com>
2021-10-04 12:41:40 +0400

    3. Тесты для определения расстояния между точками. [#159459 #211726]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,7 +7,10 @@ public class PointTest {
 
     @Test
     public void when00To20then2() {
-        int x1 = 0, y1 = 0, x2 = 2, y2 = 0;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
         double expected = 2;
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
@@ -15,7 +18,10 @@ public class PointTest {
 
     @Test
     public void when32To82then5() {
-        int x1 = 3, y1 = 2, x2 = 8, y2 = 2;
+        int x1 = 3;
+        int y1 = 2;
+        int x2 = 8;
+        int y2 = 2;
         double expected = 5;
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
@@ -23,7 +29,10 @@ public class PointTest {
 
     @Test
     public void when105To2119then18() {
-        int x1 = 10, y1 = 5, x2 = 21, y2 = 19;
+        int x1 = 10;
+        int y1 = 5;
+        int x2 = 21;
+        int y2 = 19;
         double expected = 17.8;
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
@@ -31,7 +40,10 @@ public class PointTest {
 
     @Test
     public void when00To00then0() {
-        int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 0;
         double expected = 0;
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
