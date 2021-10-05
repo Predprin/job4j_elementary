fc3efecd184bb2520b980523f02785898a754bf7
Admin <valeyk21@gmail.com>
2021-10-05 23:15:52 +0400

    4.2. Тернарное сравнение [#189 #212122]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -7,7 +7,7 @@ public class Max {
     }
 
     public static void main(String[] args) {
-        int number1 = 80;
+        int number1 = 70;
         int number2 = 19;
         int maxNumber = Max.max(number1, number2);
         System.out.println("Максимальное число: " + maxNumber);
