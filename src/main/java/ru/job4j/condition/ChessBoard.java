0bfa7e9f90745a27bd0d6da42e45d78a21a6c045
Admin <valeyk21@gmail.com>
2021-10-07 00:02:41 +0400

    4.3.6. Math.abs [#373616]

---
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+    public static boolean isPositive(int x1, int y1, int x2, int y2) {
+        return x1 >= 0 && y1 >= 0 && x2 >= 0 && y2 >= 0;
+    }
+
+    public static boolean lessSeven(int x1, int y1, int x2, int y2) {
+        return x1 <= 7 && y1 <= 7 && x2 <= 7 && y2 <= 7;
+    }
+
+    public static int way(int x1, int y1, int x2, int y2) {
+        int rsl = 0;
+        if (Math.abs(x2 - x1) == Math.abs(y2 - y1) && isPositive(x1, y1, x2, y2) && lessSeven(x1, y1, x2, y2)) {
+            rsl = Math.abs(x2 - x1);
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(way(7, 0, 0, 7));
+        System.out.println(way(8, 4, 2, 3));
+        System.out.println(way(5, -1, 3, 7));
+        System.out.println(way(2, 6, 4, 4));
+    }
+}
