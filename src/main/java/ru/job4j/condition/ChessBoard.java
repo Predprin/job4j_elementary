99e79c2ead899d674a1bf2c37518f3dcb1d169ee
Admin <valeyk21@gmail.com>
2021-10-08 09:18:22 +0400

    4.3.6. Math.abs [#373616 #213020]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -14,8 +14,6 @@ public class ChessBoard {
         if (isPositive(x1, y1, x2, y2) && lessSeven(x1, y1, x2, y2)) {
             if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
                 rsl = Math.abs(x2 - x1);
-        } else {
-            return 0;
             }
         }
         return rsl;
